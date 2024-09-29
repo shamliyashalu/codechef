027a9aa4baf170c0f5668791631766c488eb9a07
shamliyaph <shamliyashalu@gmail.com>
2024-09-29 23:20:04 +0530

    battle

---
+++ cc533.py
@@ -0,0 +1,13 @@
+x = int(input())
+y = list(map(int, input().split()))
+ev=0
+od=0
+for i in y:
+    if i%2==0:
+        ev+=1
+    else:
+        od+=1
+if ev>od:
+    print("READY FOR BATTLE")
+else:
+    print("NOT READY")
