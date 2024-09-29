21cf0c488b37a13afbee5202acfe807c443c4a88
shamliyaph <shamliyashalu@gmail.com>
2024-09-29 23:22:55 +0530

    sale season

---
+++ cc542.py
@@ -0,0 +1,12 @@
+t=int(input())
+for i in range(t):
+    x=int(input())
+    if x<=100:
+        print(x)
+    elif 100<x<=1000:
+        print(x-25)
+    elif 1000<x<=5000:
+        print(x-100)
+    else :
+        print(x-500)
+        
